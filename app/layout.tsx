0d57b2a2648f391ee979f421bd685323fffe61ea
Sofia Marchenko <marchenkosofya14@gmail.com>
2025-11-22 22:33:04 +0300

    Корзина заказов
    Реализация добавления и удаления товаров в корзину. Закрытие корзины по кнопке "Оформить заказ"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './scss/style.scss';
 import localFont from 'next/font/local';
 import Header from './ui/header';
 import Cart from './ui/cart';
+import CartProvider from './providers/CartProvider';
 
 
 const GTEestiProText = localFont({
@@ -27,13 +28,15 @@ export default function RootLayout({ children, }: Readonly<{
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
       </head>
       <body className={GTEestiProText.className}>
-        <Header />
+        <CartProvider>
+          <Header />
 
-        <main>
-          {children}
-        </main>
+          <main>
+            {children}
+          </main>
 
-        <Cart />
+          <Cart />
+        </CartProvider>
       </body>
     </html>
   );
